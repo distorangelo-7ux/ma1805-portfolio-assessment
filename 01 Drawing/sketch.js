99959897eeda5aa9d155e00ab02ce3711ea54723
distorangelo-7ux <distor.angelo7@gmail.com>
2025-10-06 14:28:55 +0100

    added folder + pong prototype

---
+++ 01 Drawing/sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(12);
+}
+
+let x = 34;
+let speed = 0.1;
+let startingHeight = 490;
+let isHit = false
+
+function draw() {
+  background(0);
+
+  //circle(25,200,10)
+  //circle(375,200,10)
+
+  rect(25,165,8,70)
+  rect(367,165,8,70)
+
+  //circle(200,200,10)
+  //square(34,198,8)
+  //square(358,198,8)
+
+  for (let y = 382; y > 0; y -= 22) {
+      noStroke()
+      rect(197, y, 6, 10);
+  }
+
+  // Calculate difference in position
+  let deltaX = speed * deltaTime
+  // Add to X position
+
+  // Reset to 0 if greater than 400
+  //if (x > 357) x = 0;
+
+  if (x < 358 && !isHit) {
+    x += deltaX
+  } else {
+    x -= deltaX
+    isHit = true
+  }
+
+  if (isHit && x < 38) {
+    isHit = false
+  }
+
+  square(x, 198, 8)
+}
