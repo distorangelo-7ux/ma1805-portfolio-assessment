4ff9a3fafbc5e135839beaf380c5a124ea5f5108
distorangelo-7ux <distor.angelo7@gmail.com>
2025-10-06 14:51:30 +0100

    renamed folder + changed bg colour

--- 00 Pong Fun/sketch.js
+++ 00 Pong Fun/sketch.js
@@ -6,43 +6,43 @@ function setup() {
 let x = 34;
 let speed = 0.1;
 let startingHeight = 490;
-let isHit = false
+let isHit = false;
 
 function draw() {
-  background(0);
+  background(56,61,64);
 
   //circle(25,200,10)
   //circle(375,200,10)
 
-  rect(25,165,8,70)
-  rect(367,165,8,70)
+  rect(25,165,8,70);
+  rect(367,165,8,70);
 
   //circle(200,200,10)
   //square(34,198,8)
   //square(358,198,8)
 
-  for (let y = 382; y > 0; y -= 22) {
-      noStroke()
+  for (let y = 384; y > 0; y -= 22) {
+      noStroke();
       rect(197, y, 6, 10);
   }
 
   // Calculate difference in position
-  let deltaX = speed * deltaTime
+  let deltaX = speed * deltaTime;
   // Add to X position
 
   // Reset to 0 if greater than 400
   //if (x > 357) x = 0;
 
   if (x < 358 && !isHit) {
-    x += deltaX
+    x += deltaX;
   } else {
-    x -= deltaX
-    isHit = true
+    x -= deltaX;
+    isHit = true;
   }
 
-  if (isHit && x < 38) {
-    isHit = false
+  if (isHit && x < 37) {
+    isHit = false;
   }
 
-  square(x, 198, 8)
+  square(x, 198, 8);
 }
