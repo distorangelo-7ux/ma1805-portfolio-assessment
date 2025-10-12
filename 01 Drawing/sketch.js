5294595419a9069a23291a09d00aff9c60a4d38a
distorangelo-7ux <distor.angelo7@gmail.com>
2025-10-12 19:07:28 +0100

    Added random scale functionality

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -1,4 +1,11 @@
 let refreshButton;
+
+let rightEyeWidth = 92;
+let rightEyeHeight = 40;
+
+let mouthWidth = 78;
+let mouthHeight = 12;
+
 let leftEyeX;
 let leftEyeY;
 
@@ -14,7 +21,7 @@ let mouthY;
 function setup() {
   createCanvas(720,720);
   refreshButton = createButton('Retake photo?')
-  refreshButton.position(600,600);
+  refreshButton.position(310,600);
 
   refreshButton.mouseClicked(getRandomTranslate);
 }
@@ -26,14 +33,21 @@ function draw() {
 
 function drawAll() {
   
+  // Left eye drawing
   translate(leftEyeX, leftEyeY);
   drawLeftEye();
+  
+  // Right eye drawing - scale is randomised as well
   translate(rightEyeX, rightEyeY);
-  drawRightEye();
+  drawRightEye(rightEyeWidth, rightEyeHeight);
+  
+  // Nose drawing
   translate(noseX, noseY);
   drawNose();
+
+  // Mouth drawing - scale is randomised as well
   translate(mouthX, mouthY)
-  drawMouth();
+  drawMouth(mouthWidth, mouthHeight);
 
 }
 
@@ -44,11 +58,17 @@ function getRandomTranslate() {
   rightEyeX = random(-7, 7);
   rightEyeY = random(-8, 9);
 
+  rightEyeWidth = random(85, 100);
+  rightEyeHeight = random(25, 62);
+
   noseX = random(-6, 7);
   noseY = random(-3, 4);
 
   mouthX = random(-7, 10);
   mouthY = random(-2, 3); 
+
+  mouthWidth = random(75, 80);
+  mouthHeight = random(5, 20);
 }
 
 function drawNose() {
@@ -60,6 +80,7 @@ function drawNose() {
 
 function drawLeftEye() {
     // Left Eye
+    // Coloured part first
   strokeWeight(0)
   fill(61, 18, 6)
   quad(
@@ -67,6 +88,7 @@ function drawLeftEye() {
      250, 275,
      250, 250,
      200, 245);
+  // Uncoloured part here
   fill(43, 20, 13)
   quad(
      250, 275,
@@ -75,14 +97,14 @@ function drawLeftEye() {
      250, 250);
 }
 
-function drawRightEye() {
+function drawRightEye(width, height) {
   // Right Eye
   ellipse(
     480,295,
-    92,40)
+    width,height)
 }
 
-function drawMouth() {
+function drawMouth(width, height) {
   fill(43, 20, 13)
-  ellipse(360,500,78,12)
+  ellipse(360,500,width,height)
 }
